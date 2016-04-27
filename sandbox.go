f830e5336ae3ef319cd6e9db5ded2c050b04f1bf
yonggang985 <yonggang985@gmail.com>
2016-04-28 00:00:56 +0800

    some exercise of golang.

---
+++ sandbox.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Printf("Welcome to the playgroud!\n")
+	fmt.Printf("The time is \n", time.Now())
+}
---
+++ type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 42.4
+	fmt.Printf("v is of type %T\n", v)
+}
